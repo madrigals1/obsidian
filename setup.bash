9717b71bd1e65667fec5307d03ae603c969d021c
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2025-12-07 23:40:28 +0100

    Add a setup script to get livesync URI with

---
+++ setup.bash
@@ -0,0 +1,10 @@
+source .env
+
+docker run \
+ -e hostname=${HOSTNAME} \
+ -e database=${COUCHDB_DATABASE} \
+ -e username=${COUCHDB_USER} \
+ -e password=${COUCHDB_PASSWORD} \
+ -e passphrase=${LIVESYNC_PASSPHRASE} \
+ docker.io/oleduc/docker-obsidian-livesync-couchdb:master \
+ deno -A /scripts/generate_setupuri.ts
